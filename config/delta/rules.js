74c751e0f97938f8a916fe4184abcb5452edc341
Martijn Bogaert <martijn.bogaert@sirus.be>
2025-03-28 13:15:53 +0100

    Remove mu-search remains (#62)
    * Remove elasticsearch
    
    * Remove mu-search
    
    * Remove mu-search config files
    
    * Remove elasticsearch scripts
    
    * Update readme
    
    * Update gitignore
    
    * Don't send deltas to elasticsearch
    
    * Remove search endpoint
    
    * Remove bpmn-element endpoints

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -13,18 +13,4 @@ export default [
       ignoreFromSelf: true,
     },
   },
-  {
-    match: {
-      // listen to all changes
-    },
-    callback: {
-      url: "http://search/update",
-      method: "POST",
-    },
-    options: {
-      resourceFormat: "v0.0.1",
-      gracePeriod: 1000,
-      ignoreFromSelf: true,
-    },
-  },
 ];
