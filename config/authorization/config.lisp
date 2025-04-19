d1d5009b64b6b0ad01795fda461401d3b206ddfc
Andreo Van Onckelen <andreo141mlg@gmail.com>
2025-04-19 04:16:35 +0200

    add uuid check

--- config/authorization/config.lisp
+++ config/authorization/config.lisp
@@ -60,6 +60,7 @@
           PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
           SELECT DISTINCT ?session_group WHERE {
             <SESSION_ID> ext:sessionGroup/mu:uuid ?session_group;
+            ?session_group mu:uuid ?uuid.
           }")
 
 (supply-allowed-group "organization-processes-editor"
