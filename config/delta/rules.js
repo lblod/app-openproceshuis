a2ba95a3eef6d9d6fcbfc448bd437dc0e25c323f
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-11-13 09:43:44 +0100

    feat: add track modified service to the app
    It will only be used for the cleanup for now as we do not use services to patch or post data
    
    Refs: oph-771

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -13,4 +13,20 @@ export default [
       ignoreFromSelf: true,
     },
   },
+  {
+    match: {
+      subject: {},
+    },
+    callback: {
+      url: "http://modified/delta",
+      method: "POST",
+    },
+    options: {
+      resourceFormat: "v0.0.1",
+      gracePeriod: 10000,
+      retry: 3,
+      ignoreFromSelf: true,
+      retryTimeout: 250,
+    },
+  },
 ];
