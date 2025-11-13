decfb01b860cee14856eaf59e03794713ff60fa5
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-11-13 17:25:14 +0100

    feat: only dispatch interesting types
    Todo also make specific type limitations possible
    
    Refs: oph-771

--- config/ldes-delta-pusher/dispatch.ts
+++ config/ldes-delta-pusher/dispatch.ts
@@ -1,12 +1,43 @@
 import { moveTriples } from "../support";
-import { Changeset } from "../types";
+import { Changeset, Quad } from "../types";
+import { ldesInstances } from './ldes-instances';
+
+import { querySudo } from "@lblod/mu-auth-sudo";
+import { sparqlEscapeUri } from "mu";
+
 
 export default async function dispatch(changesets: Changeset[]) {
+	const interestingSubjectUris: Array<string> = [];
 	for (const changeset of changesets) {
+		const interestingChanges = async (arrayOfQuads: Array<Quad>) => {
+			const interestingQuads = [];
+			for (const quad of arrayOfQuads) {
+				const subjectUri = quad.subject.value;
+				if (interestingSubjectUris.find(uri => uri === subjectUri)) {
+					interestingQuads.push(quad);
+				} else {
+					const askIfInteresting = await querySudo(`
+					ASK {
+						${sparqlEscapeUri(subjectUri)} a ?type .
+
+						VALUES ?type {
+							${Object.keys(ldesInstances).map(type => sparqlEscapeUri(type)).join('\n')}
+						}
+					}	
+				`);
+					if (Boolean(askIfInteresting?.boolean)) {
+						interestingSubjectUris.push(subjectUri);
+						interestingQuads.push(quad);
+					}
+				}
+			}
+			return interestingQuads;
+		}
+
 		await moveTriples([
 			{
-				inserts: changeset.inserts,
-				deletes: changeset.deletes,
+				inserts: await interestingChanges(changeset.inserts),
+				deletes: await interestingChanges(changeset.deletes),
 			},
 		]);
 	}
