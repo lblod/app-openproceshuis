8cad01e3fc9c9c4a294d84729b6271cc6e9aa0bb
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-11-17 16:37:03 +0100

    feat: add common filter to type so archived items do not get add to the ldes
    Refs: oph-771

--- config/ldes-delta-pusher/ldes-instances.ts
+++ config/ldes-delta-pusher/ldes-instances.ts
@@ -1,20 +1,40 @@
 export type LdesInstances = Record<string, { filter?: string }>;
 
 export const ldesInstances: LdesInstances = {
-  "https://w3id.org/dpv#Process": {},
-  "http://lblod.data.gift/vocabularies/openproceshuis/ConceptueelProces": {},
+  "https://w3id.org/dpv#Process": {
+    filter: `
+      FILTER NOT EXISTS {
+        ?s <http://www.w3.org/ns/adms#status> <http://lblod.data.gift/concepts/concept-status/gearchiveerd> .
+      }
+    `
+  },
+  "http://lblod.data.gift/vocabularies/openproceshuis/ConceptueelProces": {
+    filter: `
+      FILTER NOT EXISTS {
+        ?s <http://www.w3.org/ns/adms#status> <http://lblod.data.gift/concepts/concept-status/gearchiveerd> .
+      }
+    `
+  },
   "http://www.w3.org/2004/02/skos/core#Concept": {
     filter: `
+        FILTER NOT EXISTS {
+          ?s <http://www.w3.org/ns/adms#status> <http://lblod.data.gift/concepts/concept-status/gearchiveerd> .
+        }
         ?s <http://www.w3.org/2004/02/skos/core#inScheme> ?scheme .
         FILTER (?scheme IN(
             <http://lblod.data.gift/concept-schemes/21fba7d7-d0f5-4133-a108-626d0eb62298>, # process-category
             <http://lblod.data.gift/concept-schemes/a8108a43-44fa-4b08-9794-064941f00dc1>, # process-domain
             <http://lblod.data.gift/concept-schemes/324e775f-2a48-4daa-9de0-9f62ef8ab22e>, # process-group
-            <http://lblod.data.gift/concept-schemes/4c0f0408-01b9-4902-9640-477b667bd086>, # information-asset
-            <http://lblod.data.gift/concept-schemes/d4259f0b-6d6e-4a46-b9e1-114b774e0f1e>  # bpmn-element-types
+            <http://lblod.data.gift/concept-schemes/4c0f0408-01b9-4902-9640-477b667bd086>  # information-asset
           )
         )
       `
   },
-  "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject": {},
+  "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject": {
+    filter: `
+      FILTER NOT EXISTS {
+        ?s <http://www.w3.org/ns/adms#status> <http://lblod.data.gift/concepts/concept-status/gearchiveerd> .
+      }
+    `
+  },
 }
