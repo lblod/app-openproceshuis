4d1862d8e31539f27d05fe24c218a89e96c3809e
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-12-10 15:06:22 +0100

    feat: add the organization id after the graph name to put the process in the correct graph
    Refs: oph-772

--- config/authorization/config.lisp
+++ config/authorization/config.lisp
@@ -117,32 +117,36 @@
           }
           LIMIT 1")
 
-(supply-allowed-group "m2m-json-api"
+(supply-allowed-group "is-vendor-json-api-user"
+  :parameters ("graph_extension")
   :query "PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>    
           PREFIX muSession: <http://mu.semte.ch/vocabularies/session/>
-          SELECT ?account WHERE {
+          PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+          SELECT DISTINCT ?graph_extension
+          WHERE {
             GRAPH <http://mu.semte.ch/graphs/sessions> {
               <SESSION_ID> muSession:account ?account .
               <SESSION_ID> ext:sessionGroup ?bestuurseenheid .
+              <SESSION_ID> ext:exp ?expirationTimeInSeconds .
               FILTER(?bestuurseenheid IN(
                 <http://data.lblod.info/id/bestuurseenheden/141d9d6b-54af-4d17-b313-8d1c30bc3f5b>, # ABB - OVO001835
                 <http://data.lblod.info/id/bestuurseenheden/973a571a-2944-482f-88f2-d0491aca9859>  # Vanden Broele - 0451355351
               ))
             }
-          }"
-  :parameters ())
+            ?bestuurseenheid mu:uuid ?graph_extension .
+          }")
 
 (grant (read write)
        :to-graph sessions
-       :for-allowed-group "m2m-json-api")
+       :for-allowed-group "is-vendor-json-api-user")
 
 (grant (read write)
        :to-graph organizations-json-api
-       :for-allowed-group "m2m-json-api")
+       :for-allowed-group "is-vendor-json-api-user")
 
 (grant (read write)
-       :to-graph shared-json-api
-       :for-allowed-group "m2m-json-api")
+       :to-graph shared
+       :for-allowed-group "is-vendor-json-api-user")
 
 (grant (read)
        :to-graph public
