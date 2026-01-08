2ab384ac0bf0af8d98a0517c807036e0565435f8
Jonas Van Hoof <36266973+JonasVanHoof@users.noreply.github.com>
2026-01-08 10:18:55 +0100

    oph-854 |  Domain is changed to support sub-processes (#107)
    * feat: add new environment needed to work with mu-auth to the m2m login service
    
    Refs: oph-854
    
    * fix: migration for moving attachments of a process to be schema:media...
    
    Refs: oph-854
    
    * refactor: the ldes will not put file data objects on the ldes-feed but the ItemList and ListItems
    
    Refs: oph-854

--- config/ldes-delta-pusher/ldes-instances.ts
+++ config/ldes-delta-pusher/ldes-instances.ts
@@ -27,14 +27,11 @@ export const ldesInstances: LdesInstances = {
     graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> ?owner .',
     healingPredicates: ["http://purl.org/dc/terms/modified"],
   },
-  "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject": {
-    filter: `
-      ?s ?p ?o .
-      FILTER(?p IN (
-        <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#isPartOf>,
-        <https://schema.org/associatedMedia>
-      )) 
-    `,
+  "http://schema.org/ItemList": {
+    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> ?owner .',
+    healingPredicates: ["http://purl.org/dc/terms/modified"],
+  },
+  "http://schema.org/ListItem": {
     graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> ?owner .',
     healingPredicates: ["http://purl.org/dc/terms/modified"],
   },
