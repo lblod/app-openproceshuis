2076e49fb4184637e2afb76805d1b1a896f049e7
Martijn Bogaert <36764916+MartijnBogaert@users.noreply.github.com>
2026-05-19 15:30:43 +0200

    Introduce process versioning (#140)
    * Add versioned process model
    
    * Add versioned conceputal process model
    
    * Create versioned processes for existing canonicals
    
    * Update wrong prefix
    
    * Create versioned conceptual processes from canonicals
    
    * Remove versioned conceptual processes
    
    * OPH-1031 | extend base process versioning (#141)
    
    * feat: add owned by to all organization graphs
    
    Refs: oph-1031
    
    * refactor: only create process versions for processes that are in the owned graphs
    
    Refs: oph-1031
    
    * fix: migration naming
    
    * feat: only add items to the ldes when they are in the shared graph
    
    Refs: oph-1031
    
    * fix: we do not need to add version of to ICR resources on ldes stream
    
    Refs: oph-1031
    
    * fix: ldes-instance ownedby shared graph not public
    
    Refs: oph-1031
    
    * feat: add inverse relation on process for versions
    
    Refs: oph-1031
    
    * feat: do not show the version of on the ldes stream
    
    Refs: oph-1031
    
    * fix: canonical MUST be a string not a variable..
    
    Refs: oph-1031
    
    * refactor: use FILTER NOT IN() instead of separate ?p != X
    
    Refs: oph-1031
    
    * fix: re-add missing migration for ldes graph!
    
    Refs: oph-1031
    
    * feat: also add the ABB irregular abb graph as ownedby
    
    Refs: oph-1031
    
    * refactor: also make the graphs owned by accounts
    
    Refs: oph-1031
    
    * feat: add predicate on process to check if it is a versioned resource or not
    
    Refs: oph-1031
    
    * feat: remove inverse of versions on process
    
    Refs: oph-1031
    
    * fix: add is versioned resource flag when transforming process to versioned processes
    
    Refs: oph-1031
    
    * fix: only one versioned uri is generated per process
    
    Refs: oph-1017

--- config/ldes-delta-pusher/ldes-instances.ts
+++ config/ldes-delta-pusher/ldes-instances.ts
@@ -4,13 +4,17 @@ export const ldesInstances: LdesInstances = {
   "https://w3id.org/dpv#Process": {
     filter: `
       ?s ?p ?o .
-      FILTER(?p != <http://mu.semte.ch/vocabularies/ext/hasStatistics>)
+      FILTER(?p NOT IN (
+                        <http://mu.semte.ch/vocabularies/ext/hasStatistics>,
+                        <http://purl.org/dc/terms/isVersionOf>,
+                        <http://lblod.data.gift/vocabularies/openproceshuis/isVersionedResource>
+                      )
     `,
-    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> ?owner .',
+    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> <http://mu.semte.ch/graphs/shared> .',
     healingPredicates: ["http://purl.org/dc/terms/modified"],
   },
   "http://lblod.data.gift/vocabularies/openproceshuis/ConceptueelProces": {
-    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> ?owner .',
+    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> <http://mu.semte.ch/graphs/shared> .',
     healingPredicates: ["http://purl.org/dc/terms/modified"],
   },
   "http://www.w3.org/2004/02/skos/core#Concept": {
@@ -24,7 +28,7 @@ export const ldesInstances: LdesInstances = {
           )
         )
       `,
-    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> ?owner .',
+    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> <http://mu.semte.ch/graphs/shared> .',
     healingPredicates: ["http://purl.org/dc/terms/modified"],
   },
   "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject": {
@@ -32,19 +36,23 @@ export const ldesInstances: LdesInstances = {
       ?processOrDiagram ?pp ?s .
       FILTER(?pp IN(<http://schema.org/item>, <http://schema.org/associatedMedia>))
     `,
-    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> ?owner .',
+    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> <http://mu.semte.ch/graphs/shared> .',
     healingPredicates: ["http://purl.org/dc/terms/modified"],
   },
   "http://schema.org/ItemList": {
-    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> ?owner .',
+    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> <http://mu.semte.ch/graphs/shared> .',
     healingPredicates: ["http://purl.org/dc/terms/modified"],
   },
   "http://schema.org/ListItem": {
-    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> ?owner .',
+    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> <http://mu.semte.ch/graphs/shared> .',
     healingPredicates: ["http://purl.org/dc/terms/modified"],
   },
   "http://lblod.data.gift/vocabularies/informationclassification/InformationAsset": {
-    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> ?owner .',
+    filter: `
+      ?s ?p ?o .
+      FILTER(?p != <http://purl.org/dc/terms/isVersionOf>)
+    `,
+    graphFilter: '?g <http://mu.semte.ch/vocabularies/ext/ownedBy> <http://mu.semte.ch/graphs/shared> .',
     healingPredicates: ["http://purl.org/dc/terms/modified"],
   },
 }
