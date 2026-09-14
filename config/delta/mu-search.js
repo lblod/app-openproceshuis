412de3b7a29950ec3b320527251141fbfcfd545d
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-09-14 09:08:20 +0200

    feat: add delta rule so re-indexing is done on changes
    Refs: oph-1173

---
+++ config/delta/mu-search.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    match: {
+      // predicate: {
+      //   type: 'uri',
+      //   value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+      // },
+      // object: {
+      //   type: 'uri',
+      //   value: 'https://w3id.org/dpv#/Process',
+      // },
+    },
+    callback: {
+      url: 'http://search/update',
+      method: 'POST',
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 500,
+      ignoreFromSelf: true,
+    },
+  },
+]
